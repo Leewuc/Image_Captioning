3c2de69ea39e7c4fc59e4a6f862c39697ee537ec
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2025-01-08 02:39:27 +0900

    Add files via upload

---
+++ Image.py
@@ -0,0 +1,29 @@
+from Module import *
+
+image_path = '../input/flickr8k/Images'
+data = pd.read_csv("../input/flickr8k/captions.txt")
+data.head()
+
+
+def readImage(path, img_size=224):
+    img = load_img(path, color_mode='rgb', target_size=(img_size, img_size))
+    img = img_to_array(img)
+    img = img / 255.
+
+    return img
+
+
+def display_images(temp_df):
+    temp_df = temp_df.reset_index(drop=True)
+    plt.figure(figsize=(20, 20))
+    n = 0
+    for i in range(15):
+        n += 1
+        plt.subplot(5, 5, n)
+        plt.subplots_adjust(hspace=0.7, wspace=0.3)
+        image = readImage(f"../input/flickr8k/Images/{temp_df.image[i]}")
+        plt.imshow(image)
+        plt.title("\n".join(wrap(temp_df.caption[i], 20)))
+        plt.axis("off")
+
+display_images(data.sample(15))
